ac01c4fcad5cfab75d70c03e70b9ed9f394e4cae
dvp3011 <30864665+dvp3011@users.noreply.github.com>
2017-09-15 14:01:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,23 @@
 // counter code
 var button = document.getElementById('counter');
-var counter = 0;
 
 button.onclick = function() {
   
-  // make a request to the counter endpoint
+  // create a request object
+  var request = new XMLHttpRequest();
   
   // capture the response and store it in a variable
-  
-  // render the variable in the correct span
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
- };
+  request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE){
+          // take some action
+    if(request.status === 200) {
+        var counter = request.responseText;
+        }
+      }
+      // not done yet
+        };
+        // make the request
+        request.open('GET', 'http://dvp3011.imad.hasura-app.io/counter', true);
+        request.send(null);
+        
+   };
