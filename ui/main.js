a45a65d45710cd95420470dc5b62b89dbd074023
dvp3011 <30864665+dvp3011@users.noreply.github.com>
2017-09-15 14:05:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,6 +12,8 @@ button.onclick = function() {
           // take some action
     if(request.status === 200) {
         var counter = request.responseText;
+        var span = document.getElementById('count');
+        span.innerHTML = counter.toString();
         }
       }
       // not done yet
